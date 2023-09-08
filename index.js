c12d71460db3af1fd3d9a639aedcb403d661d69c
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-07 21:13:04 -0700

    Imported generateSVG from generateSVG.js into index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const inquirer = require('inquirer');
 const {writeFile} = require('fs').promises;
 const { Circle, Square, Triangle } = require("./lib/shapes.js");
+const generateSVG = require('./utils/generateSVG')
 
 const questions = [
 
 ];
 
 function generateSVG(){
-    
+
 }
 //TODO: this needs to follow SVG requirements
 function writeToFile(fileName, data) {
