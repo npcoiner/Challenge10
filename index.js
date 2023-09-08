2f7693f9a2d95f9750c577f71ae1f397a8bdf835
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-07 21:08:03 -0700

    Added TODO

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Circle, Square, Triangle } = require("./lib/shapes.js");
 const questions = [
 
 ];
-
+//TODO: this needs to follow SVG requirements
 function writeToFile(fileName, data) {
     const md = generateMarkdown(data);
     writeFile(fileName,md);
