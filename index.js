b1a941ae7a72d93d138a16c5b2fd9608e9fb5ce9
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-07 21:29:49 -0700

    Copied question formatting from previous assignment

--- index.js
+++ index.js
@@ -4,7 +4,36 @@ const { Circle, Square, Triangle } = require("./lib/shapes.js");
 const generateSVG = require('./utils/generateSVG')
 
 const questions = [
+
+
     //Added questions based on requirements
+    {
+        type: 'list',
+        message: "Choose a license:",
+        name: "license",
+        choices:[
+            "apache-2.0",
+            "bsl-1.0",
+            "bsd-2-clause",
+            "bsd-3-clause",
+            "cc0-1.0",
+            "cc-by-4.0",
+            "wtfpl",
+            "epl-1.0",
+            "agpl-3.0",
+            "gpl-2.0",
+            "gpl-3.0",
+            "lgpl-3.0",
+            "isc",
+            "mit",
+            "mpl-2.0",
+            "unlicense",
+            "zlib",
+        ],
+        validate: (input) => {
+            return input ? true : (console.log(this.message), false)
+        }
+    },
 ];
 
 function generateSVG(){
